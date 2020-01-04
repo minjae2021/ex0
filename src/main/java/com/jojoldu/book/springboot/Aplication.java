04bae3995a693eb2112a871768311cc7cd1632dc
Minju <minjae2021@naver.com>
2020-01-04 23:52:10 +0900

    3.3

---
+++ src/main/java/com/jojoldu/book/springboot/Aplication.java
@@ -0,0 +1,11 @@
+package com.jojoldu.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplication {
+    public static void main(String[] args){
+        SpringApplication.run(Aplication.class,args);
+    }
+}
